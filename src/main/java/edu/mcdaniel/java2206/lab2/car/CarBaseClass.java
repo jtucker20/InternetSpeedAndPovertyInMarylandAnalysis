44d0cda76b1a32941c998fba00f2d99a56e7c765
Jeremiah Tucker <57842213+jtucker20@users.noreply.github.com>
2020-07-03 09:20:46 -0400

    Initial commit

---
+++ src/main/java/edu/mcdaniel/java2206/lab2/car/CarBaseClass.java
@@ -0,0 +1,62 @@
+package edu.mcdaniel.java2206.lab2.car;
+
+public abstract class CarBaseClass {
+
+    private String make;
+    private String model;
+    private String year;
+
+    private boolean isOn;
+
+    private int numberOfWrecks;
+
+    public CarBaseClass(){
+        make = "";
+        model = "";
+        year = "2020";
+        isOn = false;
+        this.numberOfWrecks = 0;
+    }
+
+    public abstract String wreckReport();
+
+    public int getNumberOfWrecks(){
+        return this.numberOfWrecks;
+    }
+
+    public void setNumberOfWrecks(int numberOfWrecks) {
+        this.numberOfWrecks = numberOfWrecks;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public boolean isOn() {
+        return isOn;
+    }
+
+    public void setOn(boolean on) {
+        isOn = on;
+    }
+}
---
+++ src/main/java/edu/mcdaniel/java2206/lab2/vehicles/ToyotaVehicle.java
@@ -0,0 +1,29 @@
+package edu.mcdaniel.java2206.lab2.vehicles;
+
+import edu.mcdaniel.java2206.lab2.car.CarBaseClass;
+
+public abstract class ToyotaVehicle extends CarBaseClass {
+
+    private double speed;
+
+    public ToyotaVehicle(){
+        super();
+        setMake("Toyota");
+        speed = 0;
+    }
+
+    public String displaySpeedAsString(){
+        return "This Toyota's speed is: " + speed;
+    }
+
+    public abstract String getWreckReportForToyota();
+
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+}
